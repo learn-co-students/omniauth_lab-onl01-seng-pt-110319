24b5295002908c0bfddf3038acf5de7b66dd3184
Matthew Jones <matts3371@gmail.com>
2020-05-03 13:47:58 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # Add your routes here
+  root 'welcome#home'
+  
+  get '/auth/facebook/callback' => 'sessions#create'
+  delete '/', to: 'sessions#destroy', as: 'logout'
 end
