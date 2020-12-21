92fd575be389ae551a85d048dcd4bb1300c0698c
wills blake <richwblake@gmail.com>
2020-12-20 20:11:45 -0500

    Changed line 43 in gemfile to include github strategy gem per the recommended protocol on the github strategy repo's README.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
   # Add your routes here
-  match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
-  get '/', to: 'welcome#home', as: :root
 end
